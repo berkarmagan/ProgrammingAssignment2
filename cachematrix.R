9859b6e115e77045c23d1aea746aa03f5d6ee0d7
berkarmagan <45390892+berkarmagan@users.noreply.github.com>
2019-02-13 17:11:15 +0300

    cachematrix.R
    cachematrix.R by berkarmagan

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,30 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## makeCacheMatrix is a func and needs a matrix to evaluate. It has basicly 2 func; setinverse and getinverse. Setinverse will use in the
+## cacheSolve func if it s the 1st time for trying to get inverse for that session. If it s not the first time then getinverse will use 
+## in the cacheSolve which stores the already inversed data.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  t <- NULL
+  set <- function(y) {x <<- y
+    t <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) t <<- inverse
+  getinverse <- function() t
+  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  t <- x$getinverse()
+  if (!is.null(t)) {message("getting cached data")
+    return(t)
+  }
+  dataset <- x$get()
+  t <- solve(dataset, ...)
+  x$setinverse(t)
+  t
 }
+
+
+
